2ecb609fee772ff7c2ee0373c009f08525d2a4f3
Ethan Olsen <131eolsen@gmail.com>
2023-05-19 22:21:57 -0700

    chore: init

---
+++ src/lib/Editor.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { Stage, Layer, Rect, Text, Image } from 'svelte-konva';
+	import {
+		type CanvasElement,
+		type CanvasElementType,
+		canvasElements
+	} from '$stores/canvasElements';
+</script>
+
+<Stage config={{ width: window.innerWidth, height: window.innerHeight }}>
+	<Layer>
+		{#each $canvasElements as element}
+			{#if element?.type === 'Text'}
+				<Text config={element?.config ?? {}} />
+			{:else if element?.type === 'Image'}
+				<Image config={element?.config ?? {}} />
+			{/if}
+		{/each}
+	</Layer>
+</Stage>
