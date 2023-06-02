0a145729df5af06ae1286003fca86a3a032b38bf
Ethan Olsen <131eolsen@gmail.com>
2023-06-02 09:02:31 -0700

    chore: add bug icon for errors

--- src/lib/svgs/SvgIcon.svelte
+++ src/lib/svgs/SvgIcon.svelte
@@ -2,11 +2,12 @@
 	import type { CanvasElementType } from '$stores/canvasElementsTypes';
 	import SvgAddIcon from './SvgAddIcon.svelte';
 	import SvgBackIcon from './SvgBackIcon.svelte';
+	import SvgBugIcon from './SvgBugIcon.svelte';
 	import SvgCloseIcon from './SvgCloseIcon.svelte';
 	import SvgImageIcon from './SvgImageIcon.svelte';
 	import SvgTextIcon from './SvgTextIcon.svelte';
 
-	export let type: CanvasElementType | 'New' | 'Close' | 'Back' = 'Image';
+	export let type: CanvasElementType | 'New' | 'Close' | 'Back' | 'Bug' = 'Image';
 </script>
 
 {#if type === 'Image'}
@@ -19,4 +20,6 @@
 	<SvgCloseIcon />
 {:else if type === 'Back'}
 	<SvgBackIcon />
+{:else if type === 'Bug'}
+	<SvgBugIcon />
 {/if}
