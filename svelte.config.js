b23522bd9052ae134a09be9536278e7e360f4169
Ethan Olsen <131eolsen@gmail.com>
2023-06-09 22:41:54 -0700

    chore: add zod and pdfmake, add schema alias

--- svelte.config.js
+++ svelte.config.js
@@ -19,6 +19,7 @@ const config = {
 			$stores: './src/stores',
 			$utils: './src/utils',
 			$services: './src/services',
+			$schemas: './src/schemas',
 		},
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
